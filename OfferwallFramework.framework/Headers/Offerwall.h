395b6949a7d64580430d7b6690842223fb5b772b
Antonio Luna <antonio.luna@tagwizz.com.mx>
2020-08-26 11:00:05 -0700

    First Release

---
+++ OfferwallFramework.framework/Headers/Offerwall.h
@@ -0,0 +1,21 @@
+//
+//  Offerwall.h
+//  OfferwallFramework
+//
+//  Created by Antonio Luna on 8/24/20.
+//  Copyright © 2020 Techerus. All rights reserved.
+//
+
+#ifndef Offerwall_h
+#define Offerwall_h
+
+#import <UIKit/UIKit.h>
+
+@interface Offerwall : NSObject
+
++ (void) launchOfferwall:(UIViewController*) rootViewController;
++ (void) closeOfferwall;
+
+@end
+
+#endif /* Offerwall_h */
---
+++ OfferwallFramework.framework/Headers/OfferwallFramework.h
@@ -0,0 +1,14 @@
+//
+//  OfferwallFramework.h
+//  OfferwallFramework
+//
+//  Created by Antonio Luna on 8/24/20.
+//  Copyright © 2020 Techerus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Offerwall.h"
+
+FOUNDATION_EXPORT double OfferwallFrameworkVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char OfferwallFrameworkVersionString[];
